03a14603c54489051128a7d35621b576d7b00736
Izabela Tavares <belabh2003yahoo@gmail.com>
2023-03-29 10:34:05 -0300

    .

--- makefile
+++ makefile
@@ -1,28 +1,69 @@
-# CC and flags
 CC = g++
-CXXFLAGS = -std=c++11 -g -Wall
+LIBS = -lm
+SRC = src
+OBJ = obj
+INC = include
+BIN = bin
+OBJS = $(OBJ)/main.o $(OBJ)/fatorial.o $(OBJ)/fibonacci.o
+HDRS = $(INC)/fatorial.h $(INC)/fibonacci.h $(INC)/msgassert.h
+CFLAGS = -Wall -c -I$(INC)
 
-# folders
-INCLUDE_FOLDER = ./include/
-BIN_FOLDER = ./bin/
-OBJ_FOLDER = ./obj/
-SRC_FOLDER = ./src/
+EXE = $(BIN)/run
 
-# all sources, objs, and header files
-MAIN = main
-TARGET = run
-SRC = $(wildcard $(SRC_FOLDER)*.cpp)
-OBJ = $(patsubst $(SRC_FOLDER)%.cpp, $(OBJ_FOLDER)%.o, $(SRC))
+all:  $(EXE)
+	$(EXE) -t 1
+	$(EXE) -t 2
+	$(EXE) -t 3
+	$(EXE) -t 4
+	$(EXE) -t 5
+	$(EXE) -t 6
+	$(EXE) -t 7
+	$(EXE) -t 8
+	$(EXE) -t 9
+	$(EXE) -t 10
+	$(EXE) -t 11
+	$(EXE) -t 12
+	$(EXE) -t 13
+	$(EXE) -t 14
+	$(EXE) -t 15
+	$(EXE) -t 16
+	$(EXE) -t 17
+	$(EXE) -t 18
+	$(EXE) -t 19
+	$(EXE) -t 20
 
-# compile
-$(OBJ_FOLDER)%.o: $(SRC_FOLDER)%.cpp
-	$(CC) $(CXXFLAGS) -pg -c $< -o $@ -I$(INCLUDE_FOLDER)
+	$(EXE) -b 1
+	$(EXE) -b 2
+	$(EXE) -b 3
+	$(EXE) -b 4
+	$(EXE) -b 5
+	$(EXE) -b 6
+	$(EXE) -b 7
+	$(EXE) -b 8
+	$(EXE) -b 9
+	$(EXE) -b 10
+	$(EXE) -b 11
+	$(EXE) -b 12
+	$(EXE) -b 13
+	$(EXE) -b 14
+	$(EXE) -b 15
+	$(EXE) -b 16
+	$(EXE) -b 17
+	$(EXE) -b 18
+	$(EXE) -b 19
+	$(EXE) -b 30
 
-all: $(OBJ)
-	$(CC) $(CXXFLAGS) -pg -o $(BIN_FOLDER)$(TARGET) $(OBJ) 
+$(BIN)/run: $(OBJS)
+	$(CC) -pg -o $(BIN)/run $(OBJS) $(LIBS)
+
+$(OBJ)/main.o: $(HDRS) $(SRC)/main.cpp
+	$(CC) $(CFLAGS) -pg -o $(OBJ)/main.o $(SRC)/main.cpp 
+
+$(OBJ)/fatorial.o: $(HDRS) $(SRC)/fatorial.cpp
+	$(CC) $(CFLAGS) -pg -o $(OBJ)/fatorial.o $(SRC)/fatorial.cpp 
+
+$(OBJ)/fibonacci.o: $(HDRS) $(SRC)/fibonacci.cpp
+	$(CC) $(CFLAGS) -pg -o $(OBJ)/fibonacci.o $(SRC)/fibonacci.cpp 
 
 clean:
-	@rm -rf $(OBJ_FOLDER)* $(BIN_FOLDER)*
-
-run:
-	@$(BIN_FOLDER)$(TARGET)
+	rm -f $(EXE) $(OBJS) gmon.out
--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,118 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <ctime>
+#include <getopt.h>
 #include "fatorial.h"
 #include "fibonacci.h"
 #include "msgassert.h"
 
 using namespace std;
 
-char caso;
+// definicoes de operacoes a serem testadas
+#define OPFATORIAL 1
+#define OPFIBONACCI 2
 
-void parse_args(int argc, char const *argv[])
+typedef struct opt{
+  int opescolhida;
+  int numero;
+} opt_tipo;
+
+void uso()
+// Descricao: imprime as opcoes de uso
+// Entrada: nao tem
+// Saida: impressao das opcoes de linha de comando
 {
-  // passa o arquivo a ser lido como argumento da linha de comando
-  erroAssert(argv[1], "Sem caso selecionado");
-  caso = char(argv[1]);
+  fprintf(stderr,"OPCOES:\n");
+  fprintf(stderr,"\t-t \t\t(fatorial) \n");
+  fprintf(stderr,"\t-b \t\t(fibonacci) \n");
 }
 
-void descricao() {
-    cout << "OPÇOES:" << endl;
-    cout << "\t-t: fatorial" << endl;
-    cout << "\t-b: fibonacci" << endl;
+
+void parse_args(int argc,char ** argv, opt_tipo * opt)
+// Descricao: le as opcoes da linha de comando e inicializa variaveis
+// Entrada: argc, argv, opt
+// Saida: opt
+{
+     // variaveis externas do getopt
+     extern char * optarg;
+     extern int optind;
+
+     // variavel auxiliar
+     int c;
+
+     // inicializacao variaveis globais para opcoes
+     opt->opescolhida = -1;
+
+     // getopt - letra indica a opcao, : junto a letra indica parametro
+     // no caso de escolher mais de uma operacao, vale a ultima
+     while ((c = getopt(argc, argv, "t:b:")) != EOF){
+       switch(c) {
+         case 't':
+		  avisoAssert(opt->opescolhida==-1,
+		              "Mais de uma operacao escolhida");
+	          opt->opescolhida = OPFATORIAL;
+            opt->numero = atoi(optarg);
+                  break;
+         case 'b':
+		  avisoAssert(opt->opescolhida==-1,
+		              "Mais de uma operacao escolhida");
+	          opt->opescolhida = OPFIBONACCI;
+            opt->numero = atoi(optarg);
+                  break;
+         default:
+                  uso();
+                  exit(1);
+
+       }
+       // verificacao da consistencia das opcoes
+       erroAssert(opt->opescolhida>0,"necessario escolher operacao");
+     }
+}
+
+
+int main(int argc, char ** argv)
+// Descricao: programa principal para execucao das operacoes
+// Entrada: argc e argv
+// Saida: depende da operacao escolhida
+{
+  clock_t time_req;
+  opt_tipo opt;
+  // avaliar linha de comando
+  parse_args(argc,argv,&opt);
+
+  // execucao dependente da operacao escolhida
+  switch (opt.opescolhida){
+    case OPFATORIAL:
+         time_req = clock();
+         cout << "FATORIAL\n";
+         cout << "\tRecursivo: " << Factorial_recursive(opt.numero) << endl;
+         time_req = clock() - time_req;
+         cout << "\t\tTempo de relógio gasto -> " << 
+         ((float)time_req/CLOCKS_PER_SEC) * 1000 << " milisseconds" << endl;
+         time_req = clock();
+         cout << "\tIterativo: " << Factorial_iterative(opt.numero) << endl; 
+         time_req = clock() - time_req;
+         cout << "\t\tTempo de relógio gasto -> " << 
+         ((float)time_req/CLOCKS_PER_SEC) * 1000 << " milisseconds" << endl;
+	 break;
+    case OPFIBONACCI:
+         time_req = clock();
+         cout << "FIBONACCI\n";
+         cout << "\tRecursivo: " << Fibonacci_recursive(opt.numero) << endl; 
+         time_req = clock() - time_req;
+         cout << "\t\tTempo de relógio gasto -> " << 
+         ((float)time_req/CLOCKS_PER_SEC) * 1000 << " milisseconds" << endl;
+         time_req = clock();
+         cout << "\tIterativo: " << Fibonacci_iterative(opt.numero) << endl; 
+         cout << "\t\tTempo de relógio gasto -> " << 
+         ((float)time_req/CLOCKS_PER_SEC) * 1000 << " milisseconds" << endl;
+	 break;
+   default:
+         // nao deve ser executado, pois ha um erroAssert em parse_args
+         uso();
+	 exit(1);
+  }
+
+  return 0;
 }
